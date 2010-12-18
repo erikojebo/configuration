0404f9a15725477c25c0e698fd557fbaf610f2ff
Erik Öjebo <erik.ojebo@gmail.com>
2010-12-18 21:30:03 +0100

    Removed unused code

--- bin/directory_tree.rb
+++ bin/directory_tree.rb
@@ -23,15 +23,4 @@ class FileHierarchyReader
 
     directory_entry = DirectoryEntry.new(path, directories, files)
   end
-
-  def load_path(path)
-    @entries = []
-
-    Dir.foreach(path) do |entry|
-      next if entry == "." || entry == ".."
-      entry_path = File.join(path, entry)
-      entry = File.file?(entry) ? FileEntry.new(entry) : DirectoryEntry.new(entry_path)
-      @entries.push entry
-    end
-  end
 end
