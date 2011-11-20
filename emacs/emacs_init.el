7c29b3370e663832b031756638a46ec7597f5337
Erik Öjebo <erik.ojebo@gmail.com>
2011-11-20 20:32:07 +0100

    Added hippie-expand on tab

--- emacs/emacs_init.el
+++ emacs/emacs_init.el
@@ -152,10 +152,41 @@
 (setq yas/prompt-functions '(yas/dropdown-prompt))
 
 
+;; SmartTab
+
+(global-set-key [(tab)] 'smart-tab)
+(defun smart-tab ()
+  "This smart tab is minibuffer compliant: it acts as usual in
+    the minibuffer. Else, if mark is active, indents region. Else if
+    point is at the end of a symbol, expands it. Else indents the
+    current line."
+  (interactive)
+  (if (minibufferp)
+      (unless (minibuffer-complete)
+        (hippie-expand nil))
+    (if mark-active
+        (indent-region (region-beginning)
+                       (region-end))
+      (if (looking-at "\\_>")
+          (hippie-expand nil)
+        (indent-for-tab-command)))))
+
+;; Hippie expand
+
+(require 'hippie-exp)
+
+(setq hippie-expand-try-functions-list
+      '(yas/hippie-try-expand
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name
+        try-complete-lisp-symbol))
+
 ;; Rebindings
 (global-set-key (kbd "C-v") 'scroll-up-6-lines)
 (global-set-key (kbd "M-v") 'scroll-down-6-lines)
-(defun scroll-down-6-lines () 
+(defun scroll-down-6-lines ()
   (interactive)
   (scroll-down 6))
 (defun scroll-up-6-lines ()
