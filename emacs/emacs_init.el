15b00aec0318955a017df98717dc6adb98a43557
Erik Öjebo <erik.ojebo@gmail.com>
2012-02-15 15:49:30 +0100

    Fixed initialization of emacs in console mode

--- emacs/emacs_init.el
+++ emacs/emacs_init.el
@@ -17,7 +17,8 @@
        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
 	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
 
-(fullscreen)
+(if window-system
+    (fullscreen))
 
 ; Fix copy paste in ubuntu
 (setq x-select-enable-clipboard t)
