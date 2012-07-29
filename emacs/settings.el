ac76d14a7f78c54c4bfee7cb3ae51043bd9ebcd8
Erik Ojebo <erik.ojebo@gmail.com>
2012-07-29 23:37:48 +0200

    Fixed copy paste in cygwin

--- emacs/settings.el
+++ emacs/settings.el
@@ -22,9 +22,10 @@
 (if window-system
     (fullscreen))
 
-; Fix copy paste in ubuntu
-(setq x-select-enable-clipboard t)
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+(unless (eq system-type 'cygwin)
+                                        ; Fix copy paste in ubuntu
+  (setq x-select-enable-clipboard t)
+  (setq interprogram-paste-function 'x-cut-buffer-or-selection-value))
 
 ; try to improve slow performance on windows.
 (setq w32-get-true-file-attributes nil)
