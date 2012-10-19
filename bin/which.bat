7ea43b4a2c7bf4e3822388312dec9b6caf111b0f
Erik Öjebo <erik.ojebo@gmail.com>
2012-10-19 12:44:52 +0200

    added windows version of the which command

---
+++ bin/which.bat
@@ -0,0 +1,29 @@
+@echo off
+rem --------------------------------------------------------
+rem File: which.cmd
+rem Description: Windows equivalent of Unix which command
+rem Author: Pankaj Kumar
+rem Copyright 2004 Pankaj Kumar. All Rights Reserved.
+rem License: This software is available under GPL
+rem ---------------------------------------------------------
+setlocal
+if "%1" == "" goto noArg
+
+
+set fullpath=%~$PATH:1
+if "%fullpath%" == "" goto notFound
+echo Found in PATH: %fullpath%
+goto end
+
+
+:noArg
+echo No Argument specified
+goto end
+
+
+:notFound
+echo Argument "%1" not found in PATH
+
+
+:end
+endlocal
