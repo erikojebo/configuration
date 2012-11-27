cb6e2ce4564d01276ec644f7daa3466ad681b423
Erik Öjebo <erik.ojebo@gmail.com>
2012-11-27 22:59:55 +0100

    Rewrote cs-extract-autoprop-names to replace autoprop text in a region

--- emacs/code/cs-extract-autoprop-names.el
+++ emacs/code/cs-extract-autoprop-names.el
@@ -1,15 +1,23 @@
-(defun cs-extract-autoprop-names ()
-  (interactive)
-  (let (original-point start end)
-    (if (region-active-p)
-        (setq
-         start (region-beginning)
-         end (region-end)
-         original-point (point))
-      (setq start (point-min) end (point-max)))
-    (let ((output (replace-regexp-in-string
-                   "^.*\\b\\([_a-zA-Z]+\\)\\s-*{\\s-get\\s-*;\\s-*set\\s-*;\\s-*}.*$"
-                   "\\1"
-                   (buffer-substring start end))))
-      (with-output-to-temp-buffer "*cs-temp*"
-          (princ output)))))
+(defun cs-extract-autoprop-names-region (&optional start end)
+  (interactive "r")
+  (save-excursion
+    (goto-char start)
+    (message "start: %d, end: %d, point: %d" start end (point))
+    ;; We have to keep track of if the match is actually contained inside the
+    ;; active region. Only matches in the region should be replaced.
+    ;; End has to be updated since the position in the buffer changes when
+    ;; we replace the entire auto property match with only the property name
+    (while (and 
+            (re-search-forward
+             "^.*\\b\\([_a-zA-Z]+\\)\\s-*{\\s-get\\s-*;\\s-*set\\s-*;\\s-*}.*$"
+             nil t)
+            (message "point: %d, match-beginning: %d, match-end: %d, end: %d" (point) (match-beginning 0) (match-end 0) end)
+            (<= (match-end 0) end))
+      (let* ((total-match-length (- (match-end 0) (match-beginning 0)))
+             (property-name-length (- (match-end 1) (match-beginning 1)))
+             (deleted-char-count (- total-match-length property-name-length)))
+        (goto-char (match-beginning 0))
+        (replace-match
+         (match-string 1))
+        (setq end (- end deleted-char-count)))
+      (goto-char (match-end 0)))))
