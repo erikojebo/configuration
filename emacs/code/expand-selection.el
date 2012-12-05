fe7a5174c6cbd1d96bbfb97845a8f4fdfb011e69
Erik Öjebo <erik.ojebo@gmail.com>
2012-12-05 13:46:00 +0100

    introduced a bunch of helper locals

--- emacs/code/expand-selection.el
+++ emacs/code/expand-selection.el
@@ -12,20 +12,49 @@
 
 (defun gosu-expand-selection (&optional start end)
   (interactive "r")
-  (let* ((selection-start (if start start (mark)))
-        (selection-end (if end end (point)))
-        (selected-text (buffer-substring selection-start selection-end)))
-  (when (or (looking-from-point-at-p selection-end "-")
-            (looking-back-from-point-at-p selection-start "-"))
-      ;; (string-match-p "[a-z]*" selected-text)
-    (goto-char selection-start)
-    (skip-chars-backward "a-zA-Z|å|ä|ö|Å|Ä|Ö")
-    (setq selection-start (point))
-    (goto-char selection-end)
-    (skip-chars-forward "a-zA-Z|å|ä|ö|Å|Ä|Ö")
-    (set-mark selection-start))))
+  (let* ((whitespace-pattern "\\s-")
+         (word-char-pattern "[a-zA-Z|å|ä|ö|Å|Ä|Ö]")
+         (non-word-char-pattern "[-]")
 
-;; Kalle-
+         (selection-start (if (and start mark-active) start (point)))
+         (selection-end (if (and end mark-active) end (point)))
+         (selected-text (buffer-substring selection-start selection-end))
+
+         (whitespace-before-p (looking-back-from-point-at-p selection-start whitespace-pattern))
+         (whitespace-after-p (looking-from-point-at-p selection-end whitespace-pattern))
+         (word-char-before-p (looking-back-from-point-at-p selection-start word-char-pattern))
+         (word-char-after-p (looking-from-point-at-p selection-start word-char-pattern))
+         (non-word-char-before-p (looking-back-from-point-at-p selection-start non-word-char-pattern))
+         (non-word-char-after-p (looking-from-point-at-p selection-start non-word-char-pattern))
+
+         new-selection-start
+         new-selection-end)
+
+    (progn
+      (setq new-selection-start selection-start)
+      (setq new-selection-end selection-end)
+      (cond
+       ((and word-char-before-p word-char-after-p)
+        (goto-char selection-start)
+        (skip-chars-backward "a-zA-Z|å|ä|ö|Å|Ä|Ö")
+        (setq new-selection-start (point))
+        (goto-char selection-end)
+        (skip-chars-forward "a-zA-Z|å|ä|ö|Å|Ä|Ö")
+        (setq new-selection-end (point)))
+       
+       ((and word-char-before-p non-word-char-after-p)
+        (goto-char selection-start)
+        (skip-chars-backward "a-zA-Z|å|ä|ö|Å|Ä|Ö")
+        (setq new-selection-start (point)))
+
+       ((and non-word-char-before-p word-char-after-p)
+        (goto-char selection-end)
+        (skip-chars-forward "a-zA-Z|å|ä|ö|Å|Ä|Ö")
+        (setq new-selection-end (point))))
+      (goto-char new-selection-end)
+      (set-mark new-selection-start))))
+
+;; Nisse-Kalle-Pelle
 
 ;; K#alle => [Kalle]
 ;; Kalle-Svenson => [Kalle]-Svenson
