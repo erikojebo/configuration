95fb1c3ac5601383dbb599a4368e23830734ea75
Erik Öjebo <erik.ojebo@gmail.com>
2012-12-05 12:49:21 +0100

    Reworked implementation of basic expand selection

--- emacs/code/expand-selection.el
+++ emacs/code/expand-selection.el
@@ -1,14 +1,37 @@
+;;; Requires looking-back-from-current defined in functions.el
+
+(defun looking-back-from-point-at-p (point regex)
+  (save-excursion
+    (goto-char point)
+    (looking-back regex)))
+
+(defun looking-from-point-at-p (point regex)
+  (save-excursion
+    (goto-char point)
+    (looking-at-p regex)))
+
 (defun gosu-expand-selection (&optional start end)
   (interactive "r")
-  (let ((selection-start (if start start (mark)))
-        (selection-end (if end end (point))))
-  (when (looking-at-p "\\S-")
+  (let* ((selection-start (if start start (mark)))
+        (selection-end (if end end (point)))
+        (selected-text (buffer-substring selection-start selection-end)))
+  (when (or (looking-from-point-at-p selection-end "-")
+            (looking-back-from-point-at-p selection-start "-"))
+      ;; (string-match-p "[a-z]*" selected-text)
     (goto-char selection-start)
     (skip-chars-backward "a-zA-Z|å|ä|ö|Å|Ä|Ö")
     (setq selection-start (point))
     (goto-char selection-end)
     (skip-chars-forward "a-zA-Z|å|ä|ö|Å|Ä|Ö")
     (set-mark selection-start))))
+
+;; Kalle-
+
+;; K#alle => [Kalle]
+;; Kalle-Svenson => [Kalle]-Svenson
+;; [Kalle]-Svenson => [Kalle-Svenson]
+
+;; <>(){}[]''""
 
 ;; Om man står intill en brace/quote så expandera selection till matchande brace/quote
 ;; Om man har selection och har stött på blank space åt ena hållet men inte andra
