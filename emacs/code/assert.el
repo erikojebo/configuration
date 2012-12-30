498c5cdc2201c939b93e281d02e269588440441f
Erik Öjebo <erik.ojebo@gmail.com>
2012-12-30 22:06:48 +0100

    Added message to assert

--- emacs/code/assert.el
+++ emacs/code/assert.el
@@ -1,11 +1,30 @@
 (provide 'gosu-assert)
 
-(defmacro assert-equal (expected expression)
+(defmacro assert-equal (expected expression &optional msg)
   "Prints message if the given expression does not evaluate to the expected value"
   (eval-when-compile
     (let ((actual expression))
-      `(unless (equal ,expected ,actual)
-         (message (format "FAIL: Expected (equal %S %S). Actual value: %S"
-                          ,expected 
-                          (quote ,expression)
-                          ,actual))))))
+      `(let ((passed (equal ,expected ,actual)))
+         (if passed
+             (message ".")
+           (progn
+             (message (format "FAIL: %sExpected (equal %S %S). Actual value: %S"
+                              (if ,msg
+                                  (format "%s\n" ,msg)
+                                "")
+                              ,expected 
+                              (quote ,expression)
+                              ,actual))))
+         passed))))
+
+(defmacro with-test-buffer (content &rest body)
+  "Creates a temporary buffer with the given contents, then executes 
+the body and closes the buffer."
+  `(let ((original-buffer (current-buffer))
+        (buffer (generate-new-buffer "gosu-test-buffer")))
+     (set-buffer buffer)
+     (insert ,content)
+     ,@body
+     (set-buffer original-buffer)
+     (kill-buffer buffer)
+))
