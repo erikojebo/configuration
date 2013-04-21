13d6ed911e875602d3089ddefed225f1e9ba3963
Erik Öjebo <erik.ojebo@gmail.com>
2013-04-21 21:07:26 +0200

    Refactored and added comments to gosu-tab

--- emacs/code/tab-expansion.el
+++ emacs/code/tab-expansion.el
@@ -13,32 +13,39 @@
     point is at the end of a symbol, expands it. Else indents the
     current line."
   (interactive)
-  (cond
-   ((boundp 'ido-cur-item)
-    (ido-complete))
-   ((minibufferp)
-    (unless (minibuffer-complete)
-      (hippie-expand nil)))
-   (mark-active
-    (indent-region (region-beginning)
-                   (region-end)))
-   ((looking-at "\\_>") ;; end of a symbol?
-    (unless (hippie-expand nil) ;; try to hippie expand, otherwise just tab
-      (progn
-        (gosu-tab-mode -1)
-        (let ((binding (key-binding (kbd "TAB"))))
-          (if (commandp binding)
-              (call-interactively binding)
-          (message "No command bound to <tab>")))
-        (gosu-tab-mode t))))
-    (t (let ((gosu-tab-mode -1))
-      (progn
-        (gosu-tab-mode -1)
-        (let ((binding (key-binding (kbd "TAB"))))
-          (if (commandp binding)
-              (call-interactively binding)
-          (message "No command bound to <tab>")))
-        (gosu-tab-mode t))))))
+
+  ;; Temporarily disables gosu-tab-mode by using a let binding for the mode variable
+  ;; and then calls the command bound to TAB (e.g. the binding that would have been
+  ;; used if gosu-tab-mode had been disabled)
+  (flet ((call-original-key-binding () (let ((gosu-tab-mode nil)
+                                             (binding (key-binding (kbd "TAB"))))
+                                         (if (commandp binding)
+                                             (call-interactively binding)
+                                           (message "No command bound to TAB")))))
+    (cond
+     ;; If in ido mode, let ido do its thing
+     ((boundp 'ido-cur-item)
+      (ido-complete))
+
+     ((minibufferp)
+      (unless (minibuffer-complete)
+        (hippie-expand nil)))
+
+     ;; If the user has selected a region of text, indent the region instead of
+     ;; trying to auto-complete anything
+     (mark-active
+      (indent-region (region-beginning)
+                     (region-end)))
+
+     ;; If the cursor is at the end of a word, try to hippie expand it
+     ;; If that fails, call the original key binding insteadm
+     ((looking-at "\\_>")
+      (unless (hippie-expand nil)
+        (call-original-key-binding)))
+
+     ;; None of the special conditions apply, so just do what ever TAB would normally
+     ;; do in the current mode
+    (t (call-original-key-binding)))))
 
 (defun org-cycle-or-expand ()
   (interactive)
